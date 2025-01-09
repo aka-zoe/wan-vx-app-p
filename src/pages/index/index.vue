b44c11a807257d83de4d379d47ac02ee35fce1b6
Zoe <1415169387@qq.com>
2025-01-09 15:38:33 +0800

    feat - 首页获取文章列表与置顶文章，添加上拉加载更多功能，下拉刷新还有问题

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,53 +1,139 @@
 <template>
-  <div>
-    <button @click="login">登录</button>
-    <button @click="collect">获取收藏</button>
-    <button @click="homeList">首页文章列表</button>
-    <button @click="intoLoginPage">跳转到登录页</button>
+  <div class="root">
+
+    <div class="banner-container">
+
+      <swiper
+        class="swiper"
+        :indicator-dots="true"
+        :autoplay="true"
+        :interval="3000"
+        :duration="500"
+        @change="onSwiperChange"
+      >
+        <swiper-item v-for="(item, index) in bannerList" :key="index" class="swiper-item">
+          <img class="banner-image" :src="item.imagePath" alt="banner" />
+        </swiper-item>
+      </swiper>
+
+    </div>
+
+    <div class="scrollable-list">
+      <ul>
+        <li v-for="(item, index) in homeListData" :key="index">
+          <div class="item">
+            <p>{{ item.title }}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
-import card from "@/components/card";
-import { get, post, setToken } from "../../utils/http";
-import { toast, loading, dismiss } from "../../utils/toast";
+import toast from "../../utils/toast";
 import api from "@/api/index";
 
 export default {
   data() {
     return {
+      page: 0,          // 当前页数
+      loading: false,   // 是否正在加载
+      hasMore: true,    // 是否还有更多数据
+      bannerList: [],
+      homeListData: []
     };
   },
 
-  components: {
-    card
-  },
+  components: {},
 
   methods: {
-    async login() {
-      const res = await api.login("15061442013", "123456");
-      console.log(`登录成功：${res.username}`);
+    async getBanner() {
+      this.bannerList = await api.homeBanner();
     },
-    collect() {
-      get("message/lg/count_unread/json").then(res => {
-      }).catch(err => {
-      });
+    async fetchHomeList(isRefresh = false) {
+      if (isRefresh) {
+        // 刷新时重置数据
+        await this.homeList();
+      } else {
+        await this.loadMoreHomeList();
+      }
     },
-    homeList() {
-      api.homeDocList(1).then(res => {
-        console.log(`获取首页文章：${res.curPage}`);
-      });
+    //获取首页文章
+    async homeList() {
+      const topRes = await api.homeTopDocList();
+      const docRes = await api.homeDocList(0);
+      this.homeListData = [...topRes, ...docRes.datas];
     },
-    intoLoginPage() {
-      mpvue.reLaunch({ url: "/pages/auth/login/main" });
+    //获取分页首页文章
+    async loadMoreHomeList() {
+      const docRes = await api.homeDocList(this.page);
+      if (docRes) {
+        this.homeListData = [...this.homeListData, ...docRes.datas];
+        return true;
+      }
+      //没有数据页码-1
+      this.page--;
+      return false;
+    },
+
+    refreshList() {
+      this.page = 0;
+      this.fetchHomeList(true);
+    },
+    loadMore() {
+      this.page++;
+      this.fetchHomeList();
+    },
+    //banner轮播事件
+    onSwiperChange(event) {
     }
   },
-
+  //处理下拉刷新
+  onPullDownRefresh() {
+    this.refreshList();
+    wx.stopPullDownRefresh();
+  },
+  //处理上拉加载更多
+  onReachBottom() {
+    this.loadMore();
+  },
   created() {
-
+    this.getBanner();
+    this.fetchHomeList();
   }
 };
 </script>
 
 <style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.banner-image {
+  width: 100%;
+  height: 120px;
+}
+
+.scrollable-list {
+  flex: 1; /* 让 scrollable-list 占满剩余空间 */
+  overflow-y: auto; /* 使得容器内的内容可以垂直滚动 */
+}
+
+.scrollable-list ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 20px;
+  margin-top: 18px;
+}
 </style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-   我的
+    我的
+    <button @click="login">登录</button>
+    <button @click="collect">获取收藏</button>
   </div>
 </template>
 
 <script>
+import launch from "../../utils/launch";
+import { get } from "../../utils/http";
 
 export default {
   data() {
-    return {
-    };
+    return {};
   },
 
-  components: {
-  },
+  components: {},
 
   methods: {
+    login() {
+      launch.navigateTo("/pages/auth/login/main");
+    },
+    collect() {
+
+    },
   },
 
   created() {
