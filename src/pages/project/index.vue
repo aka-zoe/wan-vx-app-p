b09759269c860321d90ceacf33a907fcdb29ce25
Zoe <1415169387@qq.com>
2025-03-09 11:57:31 +0800

    feat - 完善项目tab功能

--- src/pages/project/index.vue
+++ src/pages/project/index.vue
@@ -1,53 +1,34 @@
 <template>
   <div>
-    <div class="horizontal-tab-list-container">
-      <scroll-view
-        class="horizontal-tab-list"
-        scroll-x
-        enable-flex
-        scroll-with-animation
-      >
+    <div class="tab-container">
+      <scroll-view class="tab-bar" scroll-x enable-flex>
         <div
+          class="tab-bar-item"
           v-for="(item, index) in tabs"
           :key="item.id"
-          class="tab-list-item"
           :class="{ active: currentTab === index }"
           @click="onTabChange(index)"
         >
-          <p class="tab-item-title" v-html="item.title"></p>
-          <!--            <div class="item-title">{{ item.title }}</div>-->
+          <p class="tab-bar-item-title" v-html="item.title"></p>
         </div>
       </scroll-view>
     </div>
 
-    <div>
+    <li class="content-list" v-for="(item,index) in contentData" :key="index">
+      <div class="content-list-item" @click="itemClick(item)">
+        <div class="image-container">
+          <img class="list-item-img" :src="item.envelopePic"></img>
+        </div>
 
-      <div class="horizontal-proj-list-container">
-        <scroll-view
-          class="scrollable-proj-list"
-          scroll-x
-          enable-flex
-          scroll-with-animation
-        >
-          <div
-            v-for="(item, index) in contentData"
-            :key="index"
-            class="child-list-item-container"
-            :class="{ active: currentTab === index }"
-            @click="onTabChange(index)"
-          >
-            <div class="child-list-item" @click="itemClick(item)">
-              <div class="child-list-item-text" style="font-size: 18px;margin-bottom: 10px">{{ item.title }}</div>
-              <img class="child-list-image" :src="item.envelopePic" alt="image" />
-              <div class="child-list-item-text" style="font-size: 14px;color: gray;">{{ item.desc }}</div>
-            </div>
-          </div>
-        </scroll-view>
+        <div class="content-list-item-column">
+          <div class="list-item-title"> {{ item.title }}</div>
+          <div> {{ item.desc }}</div>
+          <div class="list-item-date"> {{ item.niceDate }}</div>
+        </div>
 
       </div>
 
-    </div>
-
+    </li>
   </div>
 
 </template>
@@ -56,26 +37,10 @@
 
 import api from "../../api";
 import launch from "../../utils/launch";
-import iModal from 'iview-mpvue/dist/components/modal/modal'
 
 export default {
   data() {
     return {
-      'i-modal': iModal,
-      showModal:false,
-      actions3: [
-        {
-          name: '文章链接',
-          color: '#2d8cf0',
-        },
-        {
-          name: '项目源码',
-          color: '#19be6b'
-        },
-        {
-          name: '取消'
-        }
-      ],
       tabs: [], // tabs 数据
       currentTab: 0, // 当前选中的 tab 索引
       contentData: [] // 当前 tab 的内容数据
@@ -85,17 +50,6 @@ export default {
   components: {},
 
   methods: {
-    handleClick3(evt) {
-      console.log(`handleClick3`)
-      const index = evt.index
-      if (index === 0) {
-
-      }
-      else if (index === 1) {
-
-      }
-      this.showModal = false
-    },
     onTabChange(index) {
       if (this.currentTab !== index) {
         this.currentTab = index; // 更新当前 tab
@@ -104,7 +58,8 @@ export default {
       }
     },
     itemClick(item) {
-      // this.showModal = true;
+      //跳转到webview
+      launch.navigateTo("/pages/webview/main", { url: item.link });
     },
     async projectType() {
       const datas = await api.projectType();
@@ -133,22 +88,19 @@ export default {
 </script>
 
 <style scoped>
-.horizontal-tab-list-container {
+.tab-container {
   width: 100%;
   height: 40px;
-  overflow: hidden;
 }
 
-.horizontal-tab-list {
+.tab-bar {
   display: flex;
   flex-direction: row;
-  white-space: nowrap;
   height: 40px;
-  padding: 10px 0;
+  padding: 10px;
 }
 
-.tab-list-item {
-  display: inline-block;
+.tab-bar-item {
   flex-shrink: 0;
   height: 30px;
   margin-right: 10px;
@@ -157,17 +109,7 @@ export default {
   position: relative; /* 为底部横线定位 */
 }
 
-.tab-item-title {
-  margin-top: 5px;
-  font-size: 18px;
-  color: #333;
-}
-
-.list-item.active .tab-item-title {
-  color: #007aff; /* 选中状态字体颜色 */
-}
-
-.tab-list-item.active::after {
+.tab-bar-item.active::after {
   content: '';
   position: absolute;
   bottom: 0;
@@ -177,42 +119,48 @@ export default {
   background-color: #007aff; /* 底部横线颜色 */
 }
 
-.horizontal-proj-list-container {
-  width: 100%;
-  height: 900px;
-  overflow: hidden;
+.tab-bar-item-title {
+  margin-top: 5px;
+  font-size: 18px;
+  color: #333;
 }
 
-.scrollable-proj-list {
-  display: flex;
-  height: 900px;
-  flex-direction: row;
-}
-
-.child-list-item-container {
-  alignment: center;
-  width: 300px;
-  white-space: nowrap;
+.content-list {
   padding: 10px;
-  margin: 10px;
-  border: 1px solid #000; /* 圆圈边框 */
 }
 
-.child-list-item {
-  width: 300px;
+.content-list-item {
+  border-bottom: 1px dashed #ddd;
 }
 
-.child-list-item-text {
-  width: 300px;
-  word-wrap: break-word; /* 强制长单词换行 */
-  overflow-wrap: break-word; /* 浏览器兼容 */
-  white-space: normal; /* 允许换行 */
+.content-list-item-column {
+  margin-bottom: 10px;
 }
 
-.child-list-image {
-  margin-left: 25px;
-  width: 250px;
-  height: 500px;
+.image-container {
+  width: 100%; /* 或者指定宽度 */
+  height: 100px;
+  overflow: hidden; /* 超出部分隐藏 */
+  position: relative;
 }
 
+.list-item-img {
+  width: 100%; /* 让图片宽度适应容器 */
+  position: absolute;
+  top: 0; /* 从顶部显示 */
+  left: 0;
+}
+
+.list-item-title {
+  margin-top: 5px;
+  margin-bottom: 5px;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.list-item-date {
+  text-align: end;
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
 </style>
